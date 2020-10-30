39f4ae3b224d3fc6ce755a38aab90a9d714fafd9
Ruslan <ruslan.ogarkov88@gmail.com>
2020-10-30 20:11:42 +0200

    added movie elements

---
+++ src/components/elements/MovieInfo.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const MovieInfo = () => <div>Movie </div>
+
+export default MovieInfo;
---
+++ src/components/elements/MovieInfoBar.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const MovieInfoBar = () => <div>Movie info</div>
+
+export default MovieInfoBar;
