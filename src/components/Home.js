3b61c8d44fe10283f6bd943e181e6dee1d19e77f
Ruslan <ruslan.ogarkov88@gmail.com>
2020-10-30 15:12:42 +0200

    search bar ready

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,8 @@
 /* eslint-disable react/jsx-pascal-case */
 import React, { useState } from 'react';
 import {
-    API_URL,
-    API_KEY,
+    POPULAR_BASE_URL,
+    SEARCH_BASE_URL,
     IMAGE_BASE_URL,
     BACKDROP_SIZE,
     POSTER_SIZE
@@ -37,9 +37,16 @@ const Home = () => {
 
     const [searchTerm, setSearchTerm] = useState('');
 
+    const searchMovies = search => {
+        const endpoint = search ? SEARCH_BASE_URL + search : POPULAR_BASE_URL;
+
+        setSearchTerm(search);
+        fetchMovies(endpoint);
+    }
+
     const loadMoreMovies = () => {
-        const searchEndPoint = `${API_URL}search/movie?api_key=${API_KEY}&query=${searchTerm}&page=${currentPage + 1}`
-        const popularEndpoint = `${API_URL}movie/popular?api_key=${API_KEY}&page=${currentPage + 1}`;
+        const searchEndPoint = `${SEARCH_BASE_URL}${searchTerm}&page=${currentPage + 1}`
+        const popularEndpoint = `${POPULAR_BASE_URL}&page=${currentPage + 1}`;
 
         const endpoint = searchTerm ? searchEndPoint : popularEndpoint;
 
@@ -51,12 +58,14 @@ const Home = () => {
 
     return (
         <React.Fragment>
-            <HeroImage
-                image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${heroImage.backdrop_path}`}
-                title={heroImage.original_title}
-                text={heroImage.overview}
-            />
-            <SearchBar />
+            {!searchTerm && (
+                <HeroImage
+                    image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${heroImage.backdrop_path}`}
+                    title={heroImage.original_title}
+                    text={heroImage.overview}
+                />
+            )}
+            <SearchBar callback={searchMovies} />
             <Grid header={searchTerm ? 'Search Result' : 'Popular Movies'}>
                 {movies.map(movie => (
                     <MovieThumb
