2b85b126b5c51e5f2fa2a69b78ffc96e3bf5545d
Ruslan <ruslan.ogarkov88@gmail.com>
2020-10-31 15:05:17 +0200

    movie single page is ready

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -8,16 +8,36 @@ import Actor from './elements/Actor';
 import Grid from './elements/Grid';
 import Spinner from './elements/Spinner';
 
-const Movie = ({ movieId }) => (
-    <React.Fragment>
-        <Navigation />
-        <MovieInfo />
-        <MovieInfoBar />
-        <Grid>
-            <Actor />
-        </Grid>
-        <Spinner />
-    </React.Fragment>
-);
+import { useMovieFetch } from './hooks/useMovieFetch';
+
+const Movie = ({ movieId }) => {
+
+    const [movie, loading, error] = useMovieFetch(movieId);
+    console.log(movie);
+
+    if (error) return <div>Something went wrong ...</div>
+
+    if (loading) return <Spinner />
+
+
+    return (
+        <React.Fragment>
+            <Navigation movie={movie.original_title} />
+            <MovieInfo movie={movie} />
+            <MovieInfoBar
+                time={movie.runtime}
+                budget={movie.budget}
+                revenue={movie.revenue}
+            />
+            <Grid header="Actors">
+                {movie.actors.map(actor => (
+                    <Actor key={actor.credit_id} actor={actor} />
+                ))}
+            </Grid>
+        </React.Fragment>
+
+    )
+};
+
 
 export default Movie;
